3dd93dc6a195f87e671b297da4169cd68e3b8c81
Max <silverhammermba@gmail.com>
2016-03-18 15:11:59 -0400

    fix unsigned/signed comparison warning

--- random_utils.h
+++ random_utils.h
@@ -17,7 +17,7 @@ bool is_prime(unsigned int p)
 	if (p == 2) return true;
 	if (p % 2 == 0) return false;
 
-	for (int i = 3; (i * i) <= p; i += 2)
+	for (unsigned int i = 3; (i * i) <= p; i += 2)
 	{
 		if (p % i == 0) return false;
 	}
