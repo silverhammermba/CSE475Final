44414a526fc726f529d6a1e75e9c8b9766611cf1
Max <silverhammermba@gmail.com>
2016-04-09 13:50:34 -0400

    add iterator tests, refactor other tests a bit

--- test_fast_lookup_map.h
+++ test_fast_lookup_map.h
@@ -9,61 +9,97 @@
 class AFastLookupMap : public ::testing::Test
 {
 public:
-	FastLookupMap<int, int> m_table;
+	FastLookupMap<int, int> m_map;
+	std::pair<int, int> m_pair;
+
+	AFastLookupMap()
+		: m_pair {5, 6}
+	{
+	}
 };
 
 TEST_F(AFastLookupMap, IsEmptyWhenCreated)
 {
-	EXPECT_EQ(0u, m_table.size());
+	EXPECT_EQ(0u, m_map.size());
+	ASSERT_EQ(m_map.begin(), m_map.end());
 }
 
 TEST_F(AFastLookupMap, CannotAccessMissingPair)
 {
-	int k{ 5 };
-	EXPECT_EQ(0u, m_table.count(k));
-	EXPECT_THROW(m_table.at(k), std::out_of_range);
+	EXPECT_EQ(0u, m_map.count(m_pair.first));
+	EXPECT_THROW(m_map.at(m_pair.first), std::out_of_range);
 }
 
 TEST_F(AFastLookupMap, CanInsertPair)
 {
-	int k{ 5 };
-	int v{ 6 };
-	ASSERT_EQ(true, m_table.insert(std::make_pair(k, v)));
-	EXPECT_EQ(1u, m_table.size());
-	EXPECT_EQ(1u, m_table.count(k));
-	EXPECT_EQ(v, m_table.at(k));
+	ASSERT_EQ(true, m_map.insert(m_pair));
+	EXPECT_EQ(1u, m_map.size());
+	EXPECT_EQ(1u, m_map.count(m_pair.first));
+	EXPECT_EQ(m_pair.second, m_map.at(m_pair.first));
 }
 
 TEST_F(AFastLookupMap, CanErasePair)
 {
-	int k{ 5 };
-	ASSERT_EQ(0u, m_table.erase(k));
+	ASSERT_EQ(0u, m_map.erase(m_pair.first));
 
-	m_table.insert(std::make_pair(k, k + 1));
+	m_map.insert(m_pair);
 
-	EXPECT_EQ(1u, m_table.erase(k));
-	EXPECT_EQ(0u, m_table.count(k));
-	EXPECT_EQ(0u, m_table.size());
+	EXPECT_EQ(1u, m_map.erase(m_pair.first));
+	EXPECT_EQ(0u, m_map.count(m_pair.first));
+	EXPECT_EQ(0u, m_map.size());
+}
+
+TEST_F(AFastLookupMap, IsIterable)
+{
+	m_map.insert(m_pair);
+
+	// first value should be what we just inserted
+	auto it = m_map.begin();
+	ASSERT_EQ((*it).first, m_pair.first);
+	ASSERT_EQ((*it).second, m_pair.second);
+
+	// we should be able to change it
+	// TODO need to get -> working
+	++(*it).second;
+	ASSERT_EQ(m_pair.second + 1, m_map.at(m_pair.first));
+
+	// next value should be the end
+	ASSERT_EQ(++it, m_map.end());
+}
+
+TEST_F(AFastLookupMap, CanUseRangeBasedFor)
+{
+	m_map.insert(m_pair);
+
+	for (auto& p : m_map)
+	{
+		ASSERT_EQ(p.first, m_pair.first);
+		ASSERT_EQ(p.second, m_pair.second);
+
+		++p.second;
+	}
+
+	ASSERT_EQ(m_pair.second + 1, m_map.at(m_pair.first));
 }
 
 TEST_F(AFastLookupMap, CanInsertManyPairs)
 {
-	size_t count = 1000;
+	int count = 1000;
 
 	std::vector<std::pair<int, int>> pairs;
-	for (size_t i = 0; i < count; ++i)
-		pairs.push_back(std::make_pair(int(i), -int(i)));
+	for (int i = 0; i < count; ++i)
+		pairs.push_back(std::make_pair(i, -i));
 
-	for (const auto& pair : pairs) ASSERT_EQ(true, m_table.insert(pair));
+	for (const auto& pair : pairs) ASSERT_EQ(true, m_map.insert(pair));
 
-	// table rebuilds during inserts, so only tests elements after all inserts complete
+	// table rebuilds during inserts, so only test elements after all inserts complete
 
-	EXPECT_EQ(count, m_table.size());
+	EXPECT_EQ(size_t(count), m_map.size());
 
 	for (const auto& pair : pairs)
 	{
-		EXPECT_EQ(1u, m_table.count(pair.first));
-		EXPECT_EQ(pair.second, m_table.at(pair.first));
+		EXPECT_EQ(1u, m_map.count(pair.first));
+		EXPECT_EQ(pair.second, m_map.at(pair.first));
 	}
 }
 
