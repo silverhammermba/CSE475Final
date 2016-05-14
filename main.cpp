d1b561affeb154e6e7cafeac91d3e66c68f086e6
Max <silverhammermba@gmail.com>
2016-05-14 19:19:42 -0400

    greatly simplify speed test
    just outputs the clock cycles, no more test config since args are now
    mandatory, use random_uint for randomness in the test, also make
    random_uint thread_local on linux

--- main.cpp
+++ main.cpp
@@ -1,15 +1,13 @@
 #include <iostream>
-#include <string>
 #include <boost/program_options.hpp>
 
 #include "gtest_include.h"
 #include "test_fast_lookup_map.h"
 #include "test_fast_map.h"
-#include "config_t.h"
-#include "tests.h"
+#include "speed_test.h"
 
-// TODO:	Figure out constants c and s(M),M relationship
-//			When # partitions decreases, is it better to reduce memory allocation or to track separate partition count
+// TODO: Figure out constants c and s(M),M relationship
+//       When # partitions decreases, is it better to reduce memory allocation or to track separate partition count
 namespace po = boost::program_options;
 
 int main(int argc, char** argv)
@@ -18,20 +16,18 @@ int main(int argc, char** argv)
 	desc.add_options()
 		("help,h", "print this message and exit")
 		("unit,u", "run unit tests instead")
-		("name,n", po::value<std::string>()->default_value("-"), "name of test run")
-		("key-max,k", po::value<int>()->default_value(2500), "upper bound of random keys")
-		("threads,t", po::value<int>()->default_value(1), "number of threads")
-		("iters,i", po::value<int>()->default_value(1000), "number of iterations per-thread")
+		("key-max,k", po::value<int>()->required(), "upper bound of random keys")
+		("threads,t", po::value<int>()->required(), "number of threads")
+		("iters,i", po::value<int>()->required(), "number of iterations per-thread")
 	;
 
 	po::variables_map options;
 	po::store(po::parse_command_line(argc, argv, desc), options);
-	po::notify(options);
 
 	if (options.count("help"))
 	{
 		std::cerr << desc << std::endl;
-		return 1;
+		return EXIT_FAILURE;
 	}
 
 	if (options.count("unit"))
@@ -40,13 +36,13 @@ int main(int argc, char** argv)
 		return RUN_ALL_TESTS();
 	}
 
-	config_t cfg(
-		options["name"].as<std::string>(),
+	po::notify(options);
+
+	std::cout << speed_test<FastMap<int, int>>(
 		options["key-max"].as<int>(),
 		options["threads"].as<int>(),
 		options["iters"].as<int>()
-	);
-	test_driver(cfg);
+	) << std::endl;
 
 	return EXIT_SUCCESS;
 }
--- random_utils.h
+++ random_utils.h
@@ -5,18 +5,6 @@
 #include <random>
 #include <stdexcept>
 
-#ifndef _MSC_VER
-#define NOEXCEPT noexcept
-#else
-#define NOEXCEPT
-#endif
-
-#if defined (_MSC_VER)
-#define thread_local __declspec( thread )
-#elif defined (__GCC__)
-#define thread_local __thread
-#endif
-
 // TODO this hard-coded prime causes infinite loops if too large, why?
 // something to do with modular arithmetic and (key % range) == 1 ?
 static const uint32_t HASH_PRIME = 2695268753U;
@@ -24,24 +12,28 @@ static const uint32_t HASH_PRIME = 2695268753U;
 // return a random size_t >= min and <= max if provided
 inline unsigned int random_uint(unsigned int min, unsigned int max = std::numeric_limits<unsigned int>::max())
 {
-	// TODO thread-safe?
-	static std::random_device random_device;
-	static std::mt19937 generator(random_device());
+#ifdef __GNUG__ // if possible make these RNGs properly thread-local
+    static thread_local std::random_device random_device;
+    static thread_local std::mt19937 generator(random_device());
+#else // just hope it works out without it
+    static std::random_device random_device;
+    static std::mt19937 generator(random_device());
+#endif
 
-	std::uniform_int_distribution<unsigned int> dist(min, max);
-	return dist(generator);
+    std::uniform_int_distribution<unsigned int> dist(min, max);
+    return dist(generator);
 }
 
 // return a random hash function onto [0, range)
 template <class K>
 std::function<size_t(K)> random_hash(size_t range)
 {
-	if (HASH_PRIME < range) throw std::out_of_range("random_hash requested range is larger than HASH_PRIME");
+    if (HASH_PRIME < range) throw std::out_of_range("random_hash requested range is larger than HASH_PRIME");
 
-	uint32_t a = random_uint(1, HASH_PRIME - 1);
-	uint32_t b = random_uint(0, HASH_PRIME - 1);
+    uint32_t a = random_uint(1, HASH_PRIME - 1);
+    uint32_t b = random_uint(0, HASH_PRIME - 1);
 
-	return [range, a, b](K key) { return ((a * key + b) % HASH_PRIME) % range; };
+    return [range, a, b](K key) { return ((a * key + b) % HASH_PRIME) % range; };
 }
 
 #endif
