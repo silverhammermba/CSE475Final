24a57a0ee8f18cf6c6cbd7b24efb70b8292fb617
Max <silverhammermba@gmail.com>
2016-05-18 18:44:16 -0400

    use rw-lock instead of coarse

--- fast_map.h
+++ fast_map.h
@@ -1,13 +1,17 @@
 #ifndef FAST_MAP_H
 #define FAST_MAP_H
 
-#include <mutex>
 #include <unordered_map>
 #include <utility>
+
+#include "rwlock.h"
 
 template <class K, class V>
 class FastMap
 {
+	typedef ReadLock read_lock_t;
+	typedef WriteLock write_lock_t;
+	typedef UpgradeLock readwrite_lock_t;
 public:
 	// construct with a hint that we need to store at least num_pairs pairs
 	FastMap(size_t num_pairs = 0)
@@ -17,35 +21,35 @@ public:
 
 	size_t size() const
 	{
-		std::lock_guard<std::mutex> lock(m_mutex);
+		read_lock_t lock(m_mutex);
 		return m_table.size();
 	}
 
 	// try to insert pair into the hash table
 	bool insert(const std::pair<const K, V>& pair)
 	{
-		std::lock_guard<std::mutex> lock(m_mutex);
+		write_lock_t lock(m_mutex);
 		return m_table.insert(pair).second;
 	}
 
 	// remove pair matching key from the table
 	size_t erase(const K& key)
 	{
-		std::lock_guard<std::mutex> lock(m_mutex);
+		write_lock_t lock(m_mutex);
 		return m_table.erase(key);
 	}
 
 	// return the value matching key
 	V at(const K& key) const
 	{
-		std::lock_guard<std::mutex> lock(m_mutex);
+		read_lock_t lock(m_mutex);
 		return m_table.at(key);
 	}
 
 	// return 1 if pair matching key is in table, else return 0
 	size_t count(const K& key) const
 	{
-		std::lock_guard<std::mutex> lock(m_mutex);
+		read_lock_t lock(m_mutex);
 		return m_table.count(key);
 	}
 
@@ -56,7 +60,7 @@ public:
 
 private:
 	std::unordered_map<K, V> m_table;
-	mutable std::mutex m_mutex;
+	mutable RWMutex m_mutex;
 };
 
 #endif
