8521a7d5186fa444cd5c5e54fd2e17d719ac38c5
Max <silverhammermba@gmail.com>
2016-05-19 10:06:05 -0400

    nicely handle all types of incorrect options
    rm unused files

--- main.cpp
+++ main.cpp
@@ -23,33 +23,34 @@ int main(int argc, char** argv)
 	;
 
 	po::variables_map options;
-	po::store(po::parse_command_line(argc, argv, desc), options);
-
-	if (options.count("help"))
-	{
-		std::cerr << desc << std::endl;
-		return EXIT_FAILURE;
-	}
 
 	try
 	{
+		po::store(po::parse_command_line(argc, argv, desc), options);
+
+		if (options.count("help"))
+		{
+			std::cerr << desc << std::endl;
+			return EXIT_FAILURE;
+		}
+
 		po::notify(options);
+
+		std::cout << speed_test<FastMap<int, int>>(
+			options["key-max"].as<int>(),
+			options["threads"].as<int>(),
+			options["iters"].as<int>(),
+			options["read"].as<int>(),
+			options["write"].as<int>(),
+			options["erase"].as<int>(),
+			options["pop"].as<int>()
+		) << std::endl;
 	}
-	catch (const po::required_option& e)
+	catch (const po::error& e)
 	{
-		std::cerr << e.what() << std::endl;
+		std::cerr << e.what() << std::endl << desc << std::endl;
 		return EXIT_FAILURE;
 	}
-
-	std::cout << speed_test<FastMap<int, int>>(
-		options["key-max"].as<int>(),
-		options["threads"].as<int>(),
-		options["iters"].as<int>(),
-		options["read"].as<int>(),
-		options["write"].as<int>(),
-		options["erase"].as<int>(),
-		options["pop"].as<int>()
-	) << std::endl;
 
 	return EXIT_SUCCESS;
 }
